c836f167b8ff034731239e90ab29cf5eb47924d2
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-27 13:41:46 -0400

    thunderbird-appointment: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1766,6 +1766,7 @@
   ./services/web-apps/lasuite-docs.nix
   ./services/web-apps/lasuite-drive.nix
   ./services/web-apps/lasuite-meet.nix
+  ./services/web-apps/thunderbird-appointment.nix
   ./services/web-apps/lauti.nix
   ./services/web-apps/lemmy.nix
   ./services/web-apps/librechat.nix
