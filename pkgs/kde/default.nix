16f3b3ad9298c901eeb1862538da472913f2e0a7
Miles Wirht <114884788+philocalyst@users.noreply.github.com>
2026-06-15 10:37:08 -0400

    ordering: fixed

--- pkgs/kde/default.nix
+++ pkgs/kde/default.nix
@@ -73,7 +73,6 @@ let
         kdiagram = self.callPackage ./misc/kdiagram { };
         kdevelop-pg-qt = self.callPackage ./misc/kdevelop-pg-qt { };
         kdsoap-ws-discovery-client = self.callPackage ./misc/kdsoap-ws-discovery-client { };
-        plasma-remotecontrollers = self.callPackage ./misc/plasma-remotecontrollers { };
         kirigami-addons = self.callPackage ./misc/kirigami-addons { };
         kio-extras-kf5 = self.callPackage ./misc/kio-extras-kf5 { };
         kio-fuse = self.callPackage ./misc/kio-fuse { };
@@ -85,6 +84,7 @@ let
         phonon = self.callPackage ./misc/phonon { };
         phonon-vlc = self.callPackage ./misc/phonon-vlc { };
         plasma-pass = self.callPackage ./misc/plasma-pass { };
+        plasma-remotecontrollers = self.callPackage ./misc/plasma-remotecontrollers { };
         plasma-wayland-protocols = self.callPackage ./misc/plasma-wayland-protocols { };
         polkit-qt-1 = self.callPackage ./misc/polkit-qt-1 { };
         pulseaudio-qt = self.callPackage ./misc/pulseaudio-qt { };
